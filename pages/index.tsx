5f02c0a723d835e14215f8c589fc3a975495f824
stark <wsd312@126.com>
2022-12-09 21:30:09 +0800

    add editconfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
           由 GPT API{' '} 强力驱动：  <a href="https://gptapi.cn">https://gptapi.cn</a>
           <code className={styles.code}>By GPT</code>
         </p>
-        <p className={styles.description}> 使用问题可以加群 <img width={300} src="/wx.png" alt="" /></p>
+        <p className={styles.description}> 使用问题可以加群 <img width={300} src="/wx3.jpeg" alt="" /></p>
         <p className={styles.description}>
           服务器已经撑爆了，如果慢，请等一会，大佬可以支持一下服务器扩容
           <img width={300} src="/5.jpeg" alt="" />
