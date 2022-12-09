473052fa50816a725b3c68204d613229b61f61ed
stark <wsd312@126.com>
2022-12-09 10:27:27 +0800

    feat: init

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          敬请期待{' '}
+          由 GPT API{' '} 强力驱动：  <a href="https://gtpapi.cn">https://gtpapi.cn</a>
           <code className={styles.code}>By GPT</code>
         </p>
 
