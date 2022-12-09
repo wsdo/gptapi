30ed321cb2a6ab15c7ba01f2b4912be0311de36f
stark <wsd312@126.com>
2022-12-09 10:40:32 +0800

    feat: init

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,8 +4,8 @@ const baidu_tongji = `
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?a24eaae5a80115d6667188a866a7d7e6";
-  var s = document.getElementsByTagName("script")[0];
+  hm.src = "https://hm.baidu.com/hm.js?6c5bf39bf1993f336474fcf9e72cc27e";
+  var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
 `;
@@ -14,17 +14,6 @@ export default function Document() {
     <Html className="antialiased [font-feature-settings:'ss01']" lang="en">
       <Head>
         <script dangerouslySetInnerHTML={{ __html: baidu_tongji }} />
-        <Script id="baidu-tongji">
-          {`
-          var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?6c5bf39bf1993f336474fcf9e72cc27e";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-          `}
-        </Script>
       </Head>
       <body className="bg-white dark:bg-slate-900">
         <Main />
