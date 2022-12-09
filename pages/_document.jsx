3d97dd42778cbafe9fb3f5e04f54c8491d314009
stark <wsd312@126.com>
2022-12-09 10:14:07 +0800

    feat: init

---
+++ pages/_document.jsx
@@ -0,0 +1,39 @@
+import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html className="antialiased [font-feature-settings:'ss01']" lang="en">
+      <Head>
+        <Script>
+          {`
+          var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6c5bf39bf1993f336474fcf9e72cc27e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+          `}
+        </Script>
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-MLSFWR0JC4');
+        `}
+        </Script>
+      </Head>
+      <body className="bg-white dark:bg-slate-900">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
